92a5b0a1cc591103dc9f76b93a26b235fc940621
Aleks Martin <aleksn41@gmail.com>
2020-04-30 16:19:26 +0200

    fixed issue server starts before server

--- backend/app.js
+++ backend/app.js
@@ -2,49 +2,53 @@ const app = require("express")()
 const db = require('./dbconnect');
 const User = require('./user');
 
-async function setup(){
-	await db.start();
-}
-setup();
 //db.search("select * from sample",(rows)=>{console.log(rows)});
+function start(){
+	app.get("/", function (req, res) {
+		res.send("ye boi");
+	});
 
-app.get("/", function (req, res) {
-    res.send("ye boi");
-});
+	//gets user-object from db
+	app.get("/user/:name", function (req, res) {
+		let userName = req.params.name;
+		let user = db.search("select * from shop_users where username='"+userName+"'", (rows)=>{
+			let user = Object.assign(new User(), rows[0])
 
-//gets user-object from db
-app.get("/user/:name", function (req, res) {
-    let userName = req.params.name;
-    let user = db.search("select * from shop_users where username='"+userName+"'", (rows)=>{
-        let user = Object.assign(new User(), rows[0])
+			res.status(200).json(user);
+		});
+	})
 
-        res.status(200).json(user);
-    });
-})
+	//login for the user
+	app.get("/login", function(req, res){
+		let userName = req.query.name;
+		let password = req.query.password;
 
-//login for the user
-app.get("/login", function(req, res){
-    let userName = req.query.name;
-    let password = req.query.password;
+		let user = db.search("select * from shop_users where username='"+userName+"'", (rows)=>{
+			let user = Object.assign(new User(), rows[0])
 
-    let user = db.search("select * from shop_users where username='"+userName+"'", (rows)=>{
-        let user = Object.assign(new User(), rows[0])
+			if (password == user.password) {
+				res.status(200).send({message:"Yes"});
+			} else {
+				res.status(418).send({message:"No"});
+			}
 
-        if (password == user.password) {
-            res.status(200).send({message:"Yes"});
-        } else {
-            res.status(418).send({message:"No"});
-        }
+		});
+	})
 
-    });
-})
+	//create new User in db
+	app.post("/user", function(req, res) {
+		let user = new User(parseInt(req.query.id), req.query.name, req.query.password, req.query.securityAnswer, "true" == req.query.admin)
+		res.status(200);
+	})
 
-//create new User in db
-app.post("/user", function(req, res) {
-    let user = new User(parseInt(req.query.id), req.query.name, req.query.password, req.query.securityAnswer, "true" == req.query.admin)
-    res.status(200);
-})
+	app.listen(8000, function () {
+		console.log("App started at localhost:8000")
+	})
+}
 
-app.listen(8000, function () {
-    console.log("App started at localhost:8000")
-})
+async function setup(callback){
+	await db.start();
+	await db.changeDatabase("shop");
+	callback();
+}
+setup(start);
