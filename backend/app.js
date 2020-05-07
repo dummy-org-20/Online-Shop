7c92240d67a1817b75cf2c853b40081bd0f89e01
Aleks Martin <aleksn41@gmail.com>
2020-05-07 13:21:59 +0200

    added cookie functionality

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,14 @@ app.use(cookieParser());
 function start(){
 	app.get("/", function (req, res) {
 		console.log(req.cookies);
-		res.cookie("test", "xd");
-		console.log(checkCookie("xd"));
-		res.send("ye boi");
+		checkCookie("lol",(user_id)=>{
+			if(user_id==null){
+				createNewCookie((cookie)=>{
+					res.cookie("test",cookie);
+					res.send("ye boi");
+				});
+			}
+		});
 	});
 
 	//gets user-object from db
@@ -48,7 +53,7 @@ function start(){
 	})
 
 	app.listen(8000, function () {
-		console.log("App started at localhost:8000")
+		console.log("App started at localhost:8000");
 	})
 }
 
@@ -56,21 +61,42 @@ function start(){
 function getImagesURL(item_id,callback){
 	db.search("SELECT url,order_id FROM shop_item_images WHERE item_id="+item_id+" ORDER BY order_id ASC",(rows)=>{
 		result=[]
-		for(i of rows[0]){
+		for(i of rows){
 			result.push(i["url"]);
 		}
 		callback(result);
 	});
 }
 
-function checkCookie(cookie){
+//checks if the cookie exists in the database and gives back the matching user_id
+function checkCookie(cookie,callback){
 	db.search("SELECT user_id FROM shop_login_cookies WHERE cookie=\""+cookie+"\"",(rows)=>{
 		if(rows.length==0){
-			return null;
+			callback(null);
 		}else{
-			return rows[0]["user_id"];
+			callback(rows[0]["user_id"]);
 		}
 	});
+}
+
+function createNewCookie(callback){
+	letters="a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,0,1,2,3,4,5,6,7,8,9".split(",");
+	string="";
+	for(let i=0;i<32;i++){
+		string+=letters[getRandomInt(31)];
+	}
+	db.search("SELECT cookie FROM shop_login_cookies WHERE cookie=\""+string+"\"",(rows)=>{
+		if(rows.length!=0){
+			createNewCookie(callback);
+		}
+		else{
+			callback(string);
+		}
+	});
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 async function setup(callback){
