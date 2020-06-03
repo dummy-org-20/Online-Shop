426ef0bd369fe3d2bf0578fde0bd2f304c7f1daf
Aleks <aleksn41@gmail.com>
2020-06-03 14:32:41 +0200

    added links to home carousel items

--- frontend/react-app/src/components/HomeCarousel.js
+++ frontend/react-app/src/components/HomeCarousel.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class HomeCarousel extends Component {
     render() {
@@ -12,28 +13,34 @@ class HomeCarousel extends Component {
                     </ol>
                     <div className="carousel-inner">
                     <div className="carousel-item active">
+                        <Link to="/details/36">
                         <img className="d-block w-100 carousel-img" src="https://media.csmobiles.com/modules/ps_imageslider/images/3d2cb66e21a025db4249169cb88c95ca267f9ee6_CSmobiles_banner_iphone11.png" alt="Second slide" />
                         <div className="carousel-caption d-none d-md-block">
                         <h4>
                             <mark>Das neue IPhone 11 hier im Shop</mark>
                         </h4>
                         </div>
+                        </Link>
                     </div>
                     <div className="carousel-item">
+                        <Link to="/products?categories=1&search=">
                         <img className="d-block w-100 carousel-img" src="http://www.sciencepublishinggroup.com/spg/decotatorfont/img/book/booklist_banner.jpg" alt="First slide" />
                         <div className="carousel-caption d-none d-md-block">
                         <h4>
                             <mark>Eine große Auswahl von Büchern</mark>
                         </h4>
                         </div>
+                        </Link>
                     </div>
                     <div className="carousel-item">
+                        <Link to="/products?categories=6&search=">
                         <img className="d-block w-100 carousel-img" src="https://media3.bosch-home.com/Images/3200x/MCIM02430361_OptiMUM_MUM9AX5S00_stage_hefekranz.jpg" alt="Third slide" />
                         <div className="carousel-caption d-none d-md-block">
                         <h4>
                             <mark>Eine große Auswahl von Küchengeräten</mark>
                         </h4>
                         </div>
+                        </Link>
                     </div>
                     </div>
                     <a className="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
